2eb19a1b84f7fe8a718467fe29d700fb90242a4f
kaizencycle <michaeljjudan@gmail.com>
2025-09-23 19:40:02 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -55,8 +55,6 @@ if DEMO_MODE:
 else:
     GIC_PER_PRIVATE = int(os.getenv("GIC_PER_PRIVATE", "10"))
     GIC_PER_PUBLISH = int(os.getenv("GIC_PER_PUBLISH", "25"))
-
-```
     REWARD_MIN_LEN  = int(os.getenv("REWARD_MIN_LEN", "200"))  
 
 # New "featured" submission marker (base reward same as publish now; weekly bonus later)
@@ -741,6 +739,7 @@ def bonus_run(req: BonusRun, x_admin_key: str = Header(default="")):
         "file": str(payout_file),
 
     }
+
 
 
 
