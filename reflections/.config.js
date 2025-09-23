3e34f0b5a02cc0f705db18d5f0705d8334e25c1d
kaizencycle <michaeljjudan@gmail.com>
2025-09-23 12:12:28 -0400

    Create .config.js

---
+++ reflections/.config.js
@@ -0,0 +1,10 @@
+```
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'standalone' // helpful for Docker
+};
+
+module.exports = nextConfig;
+
+```
