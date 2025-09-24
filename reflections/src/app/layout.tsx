e8682ac388a0a74fd18b2674394372dff97d94c5
kaizencycle <michaeljjudan@gmail.com>
2025-09-23 20:53:21 -0400

    Update layout.tsx

--- reflections/src/app/layout.tsx
+++ reflections/src/app/layout.tsx
@@ -43,12 +43,9 @@ export default function RootLayout({ children }: { children: React.ReactNode })
 }
 export default function RootLayout({ children }: { children: React.ReactNode }) 
 {
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
+  children,}: 
+{children: React.ReactNode}) 
+{return (<html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
   )
